490684120d40e0d652d28807c511a69f95122f6b
Ashraf-Bashir <eng.bashir@gmail.com>
2016-12-08 01:07:48 +0100

    Use local files for dictionary instead of static data in code

--- dutchizer.js
+++ dutchizer.js
@@ -1,18 +1,12 @@
-var translations = {
-    'altogether' : 'allemaal',
-    'course' : 'de cursus',
-    'teacher' : 'de docent',
-    'two' : 'twee',
-    'other' : 'andere',
-};
-
 var engine = {
     lastEngishWord: '',
+
     isCorrectTranslation: function ( english, dutch ) {
-        return translations[english] == dutch;
+        return dictionary.translations[english] == dutch;
     },
+
     pickAnEnglishWord: function () {
-        var englishWords = Object.keys(translations);
+        var englishWords = Object.keys(dictionary.translations);
         var randomIndex = Math.floor(Math.random() * (englishWords.length-0) );
         var englishWord = englishWords[randomIndex];
         if ( englishWord != this.lastEngishWord ) {
@@ -24,28 +18,70 @@ var engine = {
     },
 };
 
+var dictionary = {
+    translations: {},
+
+    parseFile: function(file, onsuccess) {
+        this._clearTranslations();
+        var reader = new FileReader();
+        var self = this;
+        reader.onload = function(event) {
+            self._constructTranslations(reader.result);
+            onsuccess();
+        }
+        reader.readAsText(file);
+    },
+
+    _constructTranslations: function(fileContent) {
+        var lines = fileContent.split("\n");
+        for ( var i = 0; i < lines.length; i++ ) {
+            var line = lines[i];
+            var words = line.split("\t");
+            var englishWord = words[0].trim();
+            var dutchWord   = words[1].trim();
+            this.translations[englishWord] = dutchWord;
+        }
+    },
+
+    _clearTranslations: function() {
+        this.translations = {};
+    }
+};
+
 $( document ).ready( function() {
     var $LBL_englishWord = $('#english_word');
     var $TXT_dutchWord = $('#dutch_word');
     var $LBL_result = $('#result');
+    var $FILE_dictionary = $('#dictionary_file');
+    var $DIV_translate = $('#translate_div');
 
-    var englishWord = engine.pickAnEnglishWord();
-    $LBL_englishWord.text( englishWord );
+    $FILE_dictionary[0].addEventListener('change', function(event) {
+        dictionary.parseFile(
+            $FILE_dictionary[0].files[0],
+            function() {
+                $LBL_englishWord.text( engine.pickAnEnglishWord() );
+                $DIV_translate.show();
+            }
+        );
+    });
 
     $TXT_dutchWord.on('keyup', function(event) {
         if (event.keyCode == 13) {
-            if ( engine.isCorrectTranslation( englishWord, this.value ) ) {
+            if ( engine.isCorrectTranslation( $LBL_englishWord.text(), this.value ) ) {
                 $LBL_result.text('correct');
                 setTimeout(
                     function() {
-                        englishWord = engine.pickAnEnglishWord();
-                        $LBL_englishWord.text( englishWord );
+                        $LBL_englishWord.text( engine.pickAnEnglishWord() );
                         $TXT_dutchWord.val('');
                         $LBL_result.text('');
                     }, 1000
                 );
             } else {
-                $LBL_result.text("solution is: '" + translations[englishWord] + "', Type it!");
+                $LBL_result.text(
+                    "solution is: '"
+                    + dictionary.translations[$LBL_englishWord.text()]
+                    + "', Type it please!"
+                );
                 setTimeout(
                     function() {
                         $LBL_result.text('');
